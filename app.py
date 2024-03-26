825cf056b1eb62a0d262ddd5e70fbede6c51af3e
themuku <murad.mesteliyev3@gmail.com>
2024-03-26 14:20:34 +0400

    logout and auth was implemented with http exceptions

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
-from flask import Flask, request, make_response, redirect, session, jsonify, render_template
+from flask import request, redirect, session, render_template, url_for
 import bcrypt
-
 from config import db, app
-
 from models import Blog, User
+from werkzeug.exceptions import NotFound, Unauthorized, BadRequest, Conflict, InternalServerError
 
 
 @app.route('/')
@@ -23,10 +22,10 @@ def single_blog_page(blog_id):
         if found_blog:
             return render_template("blogs_page/single_blog.html", blog=found_blog)
         else:
-            return redirect("/not-found")
+            raise NotFound
     except Exception as e:
         print(str(e))
-        return redirect("/not-found")
+        raise InternalServerError
 
 
 @app.route("/blogs")
@@ -36,49 +35,66 @@ def blogs_page():
         return render_template("blogs_page/index.html", blogs=list(blogs))
     except Exception as e:
         print(str(e))
-        return "Something went wrong"
+        return InternalServerError
 
 
-@app.route("/auth/<string:auth>", methods=["GET", "POST"])
-def auth_page(auth):
-    if request.method == "GET":
-        if auth == "login":
-            return render_template("auth_page/login.html")
-        elif auth == "sign-up":
-            return render_template("auth_page/sign-up.html")
-    elif request.method == "POST":
-        if auth == "sign-up":
-            name = request.form["username"]
-            password = request.form["password"]
-            email = request.form["email"]
+@app.route("/auth/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        email = request.form["email"]
+        password = request.form["password"]
 
-            hashed_pass = bcrypt.hashpw(password.encode("utf-8"), salt=bcrypt.gensalt())
+        found_user = User.query.filter(User.email.like(f"%{email}%")).first()
 
-            new_user = User(name=name, password_hash=hashed_pass.decode("utf-8"), email=email)
-            db.session.add(new_user)
+        if found_user and bcrypt.checkpw(password.encode("utf-8"), found_user.password_hash.encode("utf-8")):
+            session["user_id"] = found_user.id
+            return redirect(url_for("home_page"))
+        else:
+            raise BadRequest(response=redirect(url_for("sign_up")))
+    elif request.method == "GET":
+        return render_template("auth_page/login.html")
 
-            if new_user:
-                try:
-                    db.session.commit()
-                    session["user_id"] = new_user.id
-                    return redirect("/auth/login")
-                except Exception as e:
-                    print(e)
-                    redirect("/not-found")
-            else:
-                return redirect("/not-found")
 
-        elif auth == "login":
-            email = request.form["email"]
-            password = request.form["password"]
+@app.route("/auth/logout")
+def logout():
+    del session["user_id"]
 
-            found_user = User.query.filter(User.email.like(f"%{email}%")).first()
+    return redirect(url_for("home_page"))
 
-            if found_user and bcrypt.checkpw(password.encode("utf-8"), found_user.password_hash.encode("utf-8")):
-                session["user_id"] = found_user.id
-                return redirect("/")
-            else:
-                return {'errors': ['Invalid username or password. Please try again.']}, 401
+
+@app.route("/auth/sign-up", methods=["GET", "POST"])
+def sign_up():
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        email = request.form["email"]
+
+        exist_email = User.query.filter_by(email=email).first()
+        exist_username = User.query.filter_by(username=username).first()
+
+        if exist_email:
+            raise Conflict(description="User with this email is already exists")
+
+        if exist_username:
+            return Conflict(description="User with this username is already exists")
+
+        hashed_pass = bcrypt.hashpw(password.encode("utf-8"), salt=bcrypt.gensalt())
+
+        new_user = User(username=username, password_hash=hashed_pass.decode("utf-8"), email=email)
+        db.session.add(new_user)
+
+        if new_user:
+            try:
+                db.session.commit()
+                session["user_id"] = new_user.id
+                return redirect(url_for("login"))
+            except Exception as e:
+                print(e)
+                raise InternalServerError(description="Something went wrong", response=redirect(url_for("home_page")))
+        else:
+            raise InternalServerError(description="Something went wrong", response=redirect(url_for("home_page")))
+    elif request.method == "GET":
+        return render_template("auth_page/sign-up.html")
 
 
 @app.route("/create-post", methods=["POST", "GET"])
@@ -86,10 +102,14 @@ def create_post():
     if request.method == "POST":
         title = request.form["title"]
         subtitle = request.form["subtitle"]
-        author_name = request.form["author-name"]
         text = request.form["text"]
 
-        new_blog = Blog(title=title, subtitle=subtitle, author_name=author_name, text=text, user_id=session["user_id"])
+        if "user_id" in session:
+            user = User.query.get(session["user_id"])
+
+            new_blog = Blog(title=title, subtitle=subtitle, author_name=user.username, text=text, user_id=user.id)
+        else:
+            raise Unauthorized(response=redirect(url_for("login")))
 
         db.session.add(new_blog)
 
@@ -100,7 +120,7 @@ def create_post():
             return redirect(f"/blogs/{last_post.id}")
         except Exception as e:
             print(str(e))
-            return redirect("/not-found")
+            raise InternalServerError(description="Something went wrong", response=redirect(url_for("home_page")))
     elif request.method == "GET":
         return render_template("blogs_page/create_post.html")
 
--- models.py
+++ models.py
@@ -1,6 +1,20 @@
 from config import db
 from datetime import datetime
-from sqlalchemy import Column, String, Integer, Text, DateTime
+from sqlalchemy import Column, String, Integer, Text, DateTime, ForeignKey
+
+
+class User(db.Model):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    username = Column(String(100), nullable=False)
+    email = Column(String(255), nullable=False, unique=True)
+    password_hash = Column(String(255), nullable=False)
+    blogs = db.relationship("Blog", backref="author")
+    created_at = Column(DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return "<User %r>" % self.id
 
 
 class Blog(db.Model):
@@ -9,23 +23,10 @@ class Blog(db.Model):
     id = Column(Integer, primary_key=True, autoincrement=True)
     title = Column(String(255), nullable=False)
     author_name = Column(String(100), nullable=False)
-    user_id = Column(Integer, nullable=False)
+    user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
     subtitle = Column(String(255), nullable=False)
     text = Column(Text, nullable=False)
     created_at = Column(DateTime, default=datetime.utcnow)
 
     def __repr__(self):
-        return "<Article %r>" % self.id
-
-
-class User(db.Model):
-    __tablename__ = "users"
-
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    name = Column(String(100), nullable=False)
-    email = Column(String(255), nullable=False, unique=True)
-    password_hash = Column(String(255), nullable=False)
-    created_at = Column(DateTime, default=datetime.utcnow)
-
-    def __repr__(self):
-        return "<Article %r>" % self.id
+        return "<BlogPost %r>" % self.id
